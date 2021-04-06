e966e9cb11a072f016b3984133aa5b362d52f991
Eric <eric.willroth@projekt202.com>
2021-04-06 15:02:30 -0500

    fix skills scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,80 +1,93 @@
 import React, { useEffect, useState } from 'react';
-import styled, {keyframes} from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Header = props => {
-
-	const [scroll, setScroll] = useState(window.scrollY)
-	const [showMenu, setShowMenu] = useState(false)
+const Header = (props) => {
+	const [scroll, setScroll] = useState(window.scrollY);
+	const [showMenu, setShowMenu] = useState(false);
 
 	const trackScroll = () => {
 		//tracks scroll position to update navbar colors
-		document.addEventListener("scroll", () => {
-			console.log(window.scrollY)
-			if(window.scrollY>=600){
-				setScroll(600)
-			} else{
-				setScroll(0)
+		document.addEventListener('scroll', () => {
+			console.log(window.scrollY);
+			if (window.scrollY >= 600) {
+				setScroll(600);
+			} else {
+				setScroll(0);
 			}
-		})
+		});
 		//function useEffect will use to remove event listener on unMount
-		return function stopTracking(){
-			document.removeEventListener("scroll", () => {
-				console.log(window.scrollY)
-			})
-		}
-	}
+		return function stopTracking() {
+			document.removeEventListener('scroll', () => {
+				console.log(window.scrollY);
+			});
+		};
+	};
 
-	
 	const clickToScroll = (e) => {
 		document.getElementById(e.target.name).scrollIntoView({
-			behavior:'smooth', block: 'start'
-		})
-	}
+			behavior: 'smooth',
+			block: 'start',
+		});
+	};
 
 	const toggleMenu = () => {
-		setShowMenu(!showMenu)
-	}
+		setShowMenu(!showMenu);
+	};
 
-	useEffect(trackScroll, [])
+	useEffect(trackScroll, []);
 
 	return (
 		<>
 			<Nav scroll={scroll}>
 				<Links>
-					<Button name="about" onClick={clickToScroll}>About</Button>
-					<Button name="skills" onClick={clickToScroll}>Skills</Button>
-					<Button name="projects" onClick={clickToScroll}>Projects</Button>
-					<Button name="contact" onClick={clickToScroll}>Contact</Button>
+					<Button name='about' onClick={clickToScroll}>
+						About
+					</Button>
+					<Button name='skills' onClick={clickToScroll}>
+						Skills
+					</Button>
+					<Button name='projects' onClick={clickToScroll}>
+						Projects
+					</Button>
+					<Button name='contact' onClick={clickToScroll}>
+						Contact
+					</Button>
 				</Links>
 			</Nav>
 			<SmallNav scroll={scroll}>
 				<SmallNavLinks showMenu={showMenu}>
-					<Button name="about" onClick={clickToScroll}>About</Button>
-					<Button name="skills" onClick={clickToScroll}>Skills</Button>
-					<Button name="projects" onClick={clickToScroll}>Projects</Button>
-					<Button name="contact" onClick={clickToScroll}>Contact</Button>
+					<Button name='about' onClick={clickToScroll}>
+						About
+					</Button>
+					<Button name='skills' onClick={clickToScroll}>
+						Skills
+					</Button>
+					<Button name='projects' onClick={clickToScroll}>
+						Projects
+					</Button>
+					<Button name='contact' onClick={clickToScroll}>
+						Contact
+					</Button>
 					<Button onClick={toggleMenu}>X</Button>
 				</SmallNavLinks>
 				<Burger showMenu={showMenu} onClick={toggleMenu}>
-					<i class="material-icons">
-						menu
-					</i>
+					<i class='material-icons'>menu</i>
 				</Burger>
 			</SmallNav>
 		</>
 	);
-}
+};
 
 const SmallNav = styled.div`
 	display: none;
-	background: ${props => props.scroll >= 600 ? 'rgba(1,0,9,100)' : 'rgba(0, 0, 0, 0)'};
+	background: ${(props) => (props.scroll >= 600 ? 'rgba(1,0,9,100)' : 'rgba(0, 0, 0, 0)')};
 	width: 100%;
 	align-items: center;
 	justify-content: flex-end;
 	position: sticky;
 	top: 0vh;
 	height: 80px;
-	color:  #FFFFFF;
+	color: #ffffff;
 	font-family: 'Merriweather', serif;
 	font-weight: 400;
 	z-index: 1000;
@@ -84,8 +97,7 @@ const SmallNav = styled.div`
 	@media (max-width: 700px) {
 		display: none;
 	}
-
-`
+`;
 
 const Burger = styled.div`
 	display: none;
@@ -93,7 +105,7 @@ const Burger = styled.div`
 	cursor: pointer
 	margin-right: 10px;
 	@media (max-width: 1050px) {
-		display: ${props => props.showMenu ? 'none' : 'flex'};
+		display: ${(props) => (props.showMenu ? 'none' : 'flex')};
 	}
 	@media (max-width: 700px) {
 		display: none;
@@ -101,17 +113,17 @@ const Burger = styled.div`
 	i {
 		font-size: 40px;
 	}
-`
+`;
 
 const SmallNavLinks = styled.div`
-	display: ${props => props.showMenu ? 'flex' : 'none'};
+	display: ${(props) => (props.showMenu ? 'flex' : 'none')};
 	justify-content: space-evenly;
 	flex-direction: row;
 	width: 300px;
-`
+`;
 
 const Nav = styled.div`
-	background: ${props => props.scroll >= 600 ? 'rgba(1,0,9,100)' : 'rgba(0, 0, 0, 0)'};
+	background: ${(props) => (props.scroll >= 600 ? 'rgba(1,0,9,100)' : 'rgba(0, 0, 0, 0)')};
 	width: 100%;
 	display: flex;
 	align-items: center;
@@ -119,14 +131,14 @@ const Nav = styled.div`
 	position: sticky;
 	top: 0vh;
 	height: 80px;
-	color:  #FFFFFF;
+	color: #ffffff;
 	font-family: 'Merriweather', serif;
 	font-weight: 400;
 	z-index: 1000;
 	@media (max-width: 1050px) {
 		display: none;
 	}
-`
+`;
 
 const Links = styled.div`
 	display: flex;
@@ -135,7 +147,7 @@ const Links = styled.div`
 	width: 400px;
 	font-size: 22px;
 	font-family: 'Merriweather', serif;
-`
+`;
 const shake = keyframes`
 25% {
 	transform: rotate(6deg);
@@ -152,17 +164,15 @@ const shake = keyframes`
 100% {
 	transform: rotate(0deg);
 }
-`
+`;
 
 const Button = styled.button`
-	color:  #FFFFFF;
+	color: #ffffff;
 	:hover {
 		-webkit-animation: ${shake} 0.4s infinite;
 		animation: ${shake} 0.4s linear;
 		cursor: pointer;
 	}
-`
-
-
+`;
 
 export default Header;
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,106 +1,108 @@
 import React from 'react';
 import styled from 'styled-components';
-import ReactIcon from "../assets/icons/DevIcons-01.svg";
-import ReduxIcon from "../assets/icons/DevIcons-14.svg";
-import JavaScriptIcon from "../assets/icons/DevIcons-03.svg";
-import ExpressIcon from "../assets/icons/DevIcons-04.svg";
+import ReactIcon from '../assets/icons/DevIcons-01.svg';
+import ReduxIcon from '../assets/icons/DevIcons-14.svg';
+import JavaScriptIcon from '../assets/icons/DevIcons-03.svg';
+import ExpressIcon from '../assets/icons/DevIcons-04.svg';
 // import VueIcon from "../assets/icons/DevIcons-05.svg";
-import GitIcon from "../assets/icons/DevIcons-06.svg";
-import SocketIcon from "../assets/icons/DevIcons-07.svg";
-import PostgeSQLIcon from "../assets/icons/DevIcons-08.svg";
-import NodeIcon from "../assets/icons/DevIcons-09.svg";
-import NPMIcon from "../assets/icons/DevIcons-10.svg";
-import CSSIcon from "../assets/icons/DevIcons-13.svg";
-import HTMLIcon from "../assets/icons/DevIcons-15.svg";
+import GitIcon from '../assets/icons/DevIcons-06.svg';
+import SocketIcon from '../assets/icons/DevIcons-07.svg';
+import PostgeSQLIcon from '../assets/icons/DevIcons-08.svg';
+import NodeIcon from '../assets/icons/DevIcons-09.svg';
+import NPMIcon from '../assets/icons/DevIcons-10.svg';
+import CSSIcon from '../assets/icons/DevIcons-13.svg';
+import HTMLIcon from '../assets/icons/DevIcons-15.svg';
 import SassIcon from '../assets/icons/DevIcons-02.svg';
 import MaterialIcon from '../assets/icons/DevIcons-11.svg';
 
-
 const Skills = () => {
 	return (
-		<Skill id="skills">
+		<Skill>
+			<ScrollTarget id='skills'></ScrollTarget>
 			<h1>Skills</h1>
 			<IconContainer>
 				<Icon>
 					<p>React</p>
-					<a href="https://reactjs.org">
-						<img src={ReactIcon} alt="React"/>
+					<a href='https://reactjs.org'>
+						<img src={ReactIcon} alt='React' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Redux</p>
-					<a href="https://redux.js.org">
-						<img src={ReduxIcon} alt="Redux"/>
+					<a href='https://redux.js.org'>
+						<img src={ReduxIcon} alt='Redux' />
 					</a>
 				</Icon>
-				<Icon><p>ES6+</p>
-						<a href="https://es6.io">
-							<img src={JavaScriptIcon} alt="ES6+"/>
-						</a>
+				<Icon>
+					<p>ES6+</p>
+					<a href='https://es6.io'>
+						<img src={JavaScriptIcon} alt='ES6+' />
+					</a>
 				</Icon>
 				<Icon>
 					<p>Express</p>
-					<a href="https://expressjs.com">
-						<img src={ExpressIcon} alt="Express"/>
+					<a href='https://expressjs.com'>
+						<img src={ExpressIcon} alt='Express' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>NPM</p>
-					<a href="https://www.npmjs.com">
-						<img src={NPMIcon} alt="NPM"/>
+					<a href='https://www.npmjs.com'>
+						<img src={NPMIcon} alt='NPM' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Sass</p>
-					<a href="https://sass-lang.com/">
-						<img className="smallimg" src={SassIcon} alt="Sass"/>
+					<a href='https://sass-lang.com/'>
+						<img className='smallimg' src={SassIcon} alt='Sass' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Git + GitHub</p>
-					<a href="https://git-scm.com">
-						<img src={GitIcon} alt="Git + GitHub"/>
+					<a href='https://git-scm.com'>
+						<img src={GitIcon} alt='Git + GitHub' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Socket.io</p>
-					<a href="https://socket.io">
-						<img src={SocketIcon} alt="Socket.io"/>
+					<a href='https://socket.io'>
+						<img src={SocketIcon} alt='Socket.io' />
 					</a>
 				</Icon>
-				<Icon><p>PostgreSQL</p>
-					<a href="https://www.postgresql.org">
-						<img src={PostgeSQLIcon} alt="PostgreSQL"/>
+				<Icon>
+					<p>PostgreSQL</p>
+					<a href='https://www.postgresql.org'>
+						<img src={PostgeSQLIcon} alt='PostgreSQL' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Node.js</p>
-					<a href="https://nodejs.org/en">
-						<img src={NodeIcon} alt="Node.js"/>
+					<a href='https://nodejs.org/en'>
+						<img src={NodeIcon} alt='Node.js' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>CSS3</p>
-					<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3">
-						<img src={CSSIcon} alt="CSS3"/>
+					<a href='https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'>
+						<img src={CSSIcon} alt='CSS3' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>HTML5</p>
-					<a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
-						<img src={HTMLIcon} alt="HTML5"/>
+					<a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'>
+						<img src={HTMLIcon} alt='HTML5' />
 					</a>
 				</Icon>
 				<Icon>
 					<p>Material-UI</p>
-					<a href="https://material-ui.com/">
-						<img className="smallimg" src={MaterialIcon} alt="Material-UI"/>
+					<a href='https://material-ui.com/'>
+						<img className='smallimg' src={MaterialIcon} alt='Material-UI' />
 					</a>
 				</Icon>
 			</IconContainer>
 		</Skill>
 	);
-}
+};
 
 const Skill = styled.div`
 	background: #010009;
@@ -112,12 +114,12 @@ const Skill = styled.div`
 	justify-content: flex-start;
 	font-family: 'Merriweather', serif;
 	align-items: center;
-	color:  #FFFFFF;
-		h1 {
-			font-size: 30px;
-			margin-bottom: 40px;
-		}
-`
+	color: #ffffff;
+	h1 {
+		font-size: 30px;
+		margin-bottom: 40px;
+	}
+`;
 
 const IconContainer = styled.div`
 	width: 80%;
@@ -127,7 +129,7 @@ const IconContainer = styled.div`
 	@media (max-width: 700px) {
 		width: 85%;
 	}
-`
+`;
 
 const Icon = styled.div`
 	height: 140px;
@@ -161,7 +163,11 @@ const Icon = styled.div`
 				margin: 30px;
 			}
 	}
-`
+`;
 
+const ScrollTarget = styled.div`
+	position: relative;
+	top: -80px;
+`;
 
 export default Skills;
