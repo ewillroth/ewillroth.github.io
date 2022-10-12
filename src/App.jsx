c0fb47829c993bdc4d5df62f1d26b5c411a01b9d
Eric Willroth <ewillroth@gmail.com>
2022-10-11 22:18:50 -0600

    switch to yarn

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Skills from './components/Skills';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Splash from './components/Splash';
-import './reset.css'
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Splash from "./components/Splash";
+import "./reset.css";
 
 const App = () => {
-
-	return (
-	<div className="App">
-		<Header />
-		<Splash />
-		<About />
-		<Skills />
-		<Projects />
-		<Contact />
-	</div>
-	);
-}
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
 
 export default App;
